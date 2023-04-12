e83a46d4c042e1be1b9bc178dda949407a355bb3
tberry <thomas.berry@ukaea.uk>
2023-04-12 13:31:06 +0100

    Style fixes to inflammation-analysis.py with flake8

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Software for managing and analysing patients' inflammation data in our imaginary hospital."""
+"""Software for managing and analysing patients' inflammation data in our
+imaginary hospital."""
 
 import argparse
 
@@ -17,13 +18,15 @@ def main(args):
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
 
-
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
